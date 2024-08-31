e97cdf66674d1d41fa5f0f0e5a915a7a89bb6de2
claxten10 <claxten10@gmail.com>
2024-08-31 06:40:23 +0200

    hormoz: Define Architecture flags
    Change-Id: Ie290c8cdcca4120eb2cf9ca328fc47aa00b1c8fe
    Signed-off-by: claxten10 <claxten10@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -5,5 +5,11 @@
 
 DEVICE_PATH := device/daria/hormoz
 
+# Architecture
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-2a-dotprod
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_VARIANT := cortex-a76
+
 # Inherit the proprietary files
 include vendor/daria/hormoz/BoardConfigVendor.mk
